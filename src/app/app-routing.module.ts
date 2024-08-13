9951d38d87219db3c475bf580449bc3540c925d7
Nassimtoumi <nassim.toumi@esprit.tn>
2024-08-13 14:03:56 +0100

    Routes login / singin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { GrapheComponent } from './graphe/graphe.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signUp', component: SignUpComponent }
   
 ];
 
