3f486137fafecae278a766647027a3da5e119a6c
Nassimtoumi <nassim.toumi@esprit.tn>
2024-08-14 01:56:47 +0100

    strating sound

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,6 +5,22 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent  {
+export class HomeComponent  implements OnInit{
  
+  constructor() { }
+
+  ngOnInit(): void {
+    this.playOpeningSound();
+  }
+
+  playOpeningSound(): void {
+    // Crée un nouvel élément Audio
+    const audio = new Audio();
+    // Spécifie la source du fichier audio
+    audio.src = "../../assets/audio/Starting.mp3";
+    // Charge l'audio
+    audio.load();
+    // Joue l'audio
+    audio.play().catch(error => console.log('La lecture audio a échoué :', error));
+  }
 }
