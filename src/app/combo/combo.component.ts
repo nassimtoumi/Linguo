22f4e683f674b44f791c14f8b1b8f4a3adbbb4c9
Nassimtoumi <nassim.toumi@esprit.tn>
2024-08-15 12:41:20 +0100

    modification combo

--- src/app/combo/combo.component.ts
+++ src/app/combo/combo.component.ts
@@ -7,8 +7,8 @@ import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/dr
   styleUrls: ['./combo.component.css']
 })
 export class ComboComponent {
-  items = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
-  basket = ['Item 5', 'Item 6'];
+  models = ['NER ', 'STT', 'Emotion'];
+  combo = ['Punctuation','TTS '];
 
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
